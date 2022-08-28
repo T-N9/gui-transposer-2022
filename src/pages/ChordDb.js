0443868ad9cb3c934f09aff345b2e6a9c61b483a
tenyain <tenyain.mhs@gmail.com>
2022-08-28 21:13:40 +0630

    [ChordDB] test chords

--- src/pages/ChordDb.js
+++ src/pages/ChordDb.js
@@ -3,11 +3,23 @@ import React, { useState, useEffect } from "react";
 import MyChord from "../data/Chord";
 
 import C_Chords from "../chordDB/src/db/guitar/chords/C";
+import D_Chords from "../chordDB/src/db/guitar/chords/D";
+import F_Chords from "../chordDB/src/db/guitar/chords/F";
 
 const ChordDb = () => {
   const [CmajorPositions, setCmajorPositions] = useState([]);
+  const [DmajorPositions, setDmajorPositions] = useState([]);
+  const [FmajorPositions, setFmajorPositions] = useState([]);
 
   let C_major = C_Chords.filter((chord) => {
+    return chord.suffix === "major";
+  });
+
+  let D_major = D_Chords.filter((chord) => {
+    return chord.suffix === "major";
+  });
+
+  let F_major = F_Chords.filter((chord) => {
     return chord.suffix === "major";
   });
 
@@ -15,14 +27,42 @@ const ChordDb = () => {
     setCmajorPositions(C_major[0].positions);
   }, [C_major]);
 
+  useEffect(() => {
+    setDmajorPositions(D_major[0].positions);
+  }, [D_major]);
+
+  useEffect(() => {
+    setFmajorPositions(F_major[0].positions);
+  }, [F_major]);
+
   return (
     <div>
-      <h1>Chords are here.</h1>
+      <div className="container my-4">
+        <div className="my-8">
+          <h1 className="font-bold text-center">C Major chords</h1>
+          <div className="flex gap-4 justify-center">
+            {CmajorPositions.map((position, index) => {
+              return <MyChord key={index} id={index} position={position} />;
+            })}
+          </div>
+        </div>
+        <div className="my-8">
+          <h1 className="font-bold text-center">D Major chords</h1>
+          <div className="flex gap-4 justify-center">
+            {DmajorPositions.map((position, index) => {
+              return <MyChord key={index} id={index} position={position} />;
+            })}
+          </div>
+        </div>
 
-      <div className="flex gap-4">
-        {CmajorPositions.map((position, index) => {
-          return <MyChord key={index} id={index} position={position} />;
-        })}
+        <div className="my-8">
+          <h1 className="font-bold text-center">F Major chords</h1>
+          <div className="flex gap-4 justify-center">
+            {FmajorPositions.map((position, index) => {
+              return <MyChord key={index} id={index} position={position} />;
+            })}
+          </div>
+        </div>
       </div>
     </div>
   );
