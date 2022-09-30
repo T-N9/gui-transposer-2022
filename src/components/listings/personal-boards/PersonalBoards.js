3453a80d17f1cb348ebc0b0ac8804d8264215db3
T-N9 <thethoriumtension@gmail.com>
2022-09-30 21:03:04 +0630

    [Boards] code cleaning

--- src/components/listings/personal-boards/PersonalBoards.js
+++ src/components/listings/personal-boards/PersonalBoards.js
@@ -2,10 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 /* Icons */
-import {AiFillFolderOpen} from 'react-icons/ai';
+import { AiFillFolderOpen } from "react-icons/ai";
 import { Pencil2Icon } from "@radix-ui/react-icons";
 
 import Hook from "./hook.personalBoards";
+
 const PersonalBoard = () => {
   const { personalBoardList, handleBoardType } = Hook();
 
@@ -16,16 +17,19 @@ const PersonalBoard = () => {
         backgroundImage: "linear-gradient(135deg, #667eea 0%, #764ba2 100%)",
       }}
     >
-      <h1 className="font-secondary flex items-center gap-2 mb-4 text-xl font-bold text-white">
+      {/* Title */}
+      <h1 className="font-secondary flex items-center gap-2 mb-4 text-2xl font-bold text-white">
         Your Library <Pencil2Icon />
       </h1>
 
+      {/* No boards */}
       {personalBoardList.length === 0 && (
-        <p className="my-3 text-dark font-secondary">
+        <p className="my-3 text-white text-center font-secondary">
           You currently have no boards.
         </p>
       )}
 
+      {/* Board listing */}
       <div className="grid gap-4 grid-cols-1 md:grid-cols-5">
         {personalBoardList.map((item, index) => {
           return (
@@ -50,7 +54,9 @@ const PersonalBoard = () => {
                   <div className="flex justify-between items-center">
                     <p className="text-xs text-gray-400">12 / 5 /2020</p>
 
-                    <button className="primary-btn--info-outline text-lg font-bold"><AiFillFolderOpen/></button>
+                    <button className="primary-btn--info-outline text-lg font-bold">
+                      <AiFillFolderOpen />
+                    </button>
                   </div>
                 </div>
               </div>
--- src/components/listings/public-boards/PublicBoards.js
+++ src/components/listings/public-boards/PublicBoards.js
@@ -2,39 +2,55 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 /* Icons */
-import {AiFillFolderOpen} from 'react-icons/ai';
+import { AiFillFolderOpen } from "react-icons/ai";
 import { GlobeIcon } from "@radix-ui/react-icons";
 
 import Hook from "./hook.publicBoards";
 
 const PublicBoards = () => {
-  const { boardList,handleBoardType } = Hook();
+  const { boardList, handleBoardType } = Hook();
 
   return (
-    <div className="p-4 rounded-md shadow" style={{
-      // background-image: linear-gradient(60deg, #29323c 0%, #485563 100%);
-      backgroundImage: 'linear-gradient(60deg, #29323c 0%, #485563 100%)'
-    }}>
+    <div
+      className="p-4 rounded-md shadow"
+      style={{
+        backgroundImage: "linear-gradient(60deg, #29323c 0%, #485563 100%)",
+      }}
+    >
+      {/* Title */}
       <h1 className="font-secondary flex items-center gap-2 text-2xl mb-4 font-bold text-white">
-        Demo songs <GlobeIcon/>
+        Demo songs <GlobeIcon />
       </h1>
-      <div className= "board-listing--grid">
+
+      {/* Board Listing */}
+      <div className="board-listing--grid">
         {boardList.map((item, index) => {
           return (
-            <Link onClick={handleBoardType} key={index} to={`/boards/${item.id}`}>
-              <div className="p-2 w-full lg:w-auto shadow font-secondary rounded-md border-[3px] border-solid border-gray-50 bg-white min-w-[180px]">
+            <Link
+              onClick={handleBoardType}
+              key={index}
+              to={`/boards/${item.id}`}
+            >
+              <div className="listing-card">
                 <div className="flex flex-col gap-5">
                   <div>
                     <h1 className="text-xl text-light-md font-bold">
                       {item.data.songTitle}
                     </h1>
-                    <p>by <span className="text-dark font-bold">{item.data.artistName}</span></p>
+                    <p>
+                      by{" "}
+                      <span className="text-dark font-bold">
+                        {item.data.artistName}
+                      </span>
+                    </p>
                   </div>
 
                   <div className="flex justify-between items-center">
                     <p className="text-xs text-gray-400">12 / 5 /2020</p>
 
-                    <button className="primary-btn--info-outline text-lg font-bold"><AiFillFolderOpen/></button>
+                    <button className="primary-btn--info-outline text-lg font-bold">
+                      <AiFillFolderOpen />
+                    </button>
                   </div>
                 </div>
               </div>
