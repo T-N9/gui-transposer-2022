d7d782d6f2fd07efa447795a74c650c96ca49c67
T-N9 <thethoriumtension@gmail.com>
2022-10-10 15:31:00 +0630

    [ChordBoard] drag-area modified

--- src/components/common/chord-chart/ChordBoard.js
+++ src/components/common/chord-chart/ChordBoard.js
@@ -4,6 +4,9 @@ import Draggable from "react-draggable";
 /* components */
 import DefineChord from "./DefineChord";
 
+/* Icons */
+import { BsArrowsMove, BsFillXCircleFill } from "react-icons/bs";
+
 const ChordBoard = ({
   detectedChords,
   transposeLvl,
@@ -11,8 +14,17 @@ const ChordBoard = ({
   isFlat,
 }) => {
   return (
-    <Draggable>
-      <div className="chordBoard flex gap-3 fixed top-0 left-[30%] translate-x-[-50%] cursor-move p-5 shadow flex-wrap">
+    <Draggable handle="span">
+      <div className="chordBoard flex gap-3 fixed top-0 left-[30%] translate-x-[-50%] p-5 shadow flex-wrap">
+        <div className="absolute left-[-40px] top-5 flex flex-col gap-3">
+          <span className="w-8 h-8 text-white flex justify-center items-center cursor-move bg-info  rounded-full">
+            <BsArrowsMove />
+          </span>
+
+          <div className="w-8 h-8 text-white flex justify-center items-center cursor-move bg-danger  rounded-full">
+            <BsFillXCircleFill />
+          </div>
+        </div>
         {transposeLvl !== 0
           ? transposedChords.map((chord, index) => {
               return <DefineChord key={index} chord={chord} isFlat={isFlat} />;
