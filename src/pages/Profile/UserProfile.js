2a79e7ae7a75c60c8418ae7a4af7ca3baefba1bd
T-N9 <thethoriumtension@gmail.com>
2022-10-11 08:58:39 +0630

    [Profile] design updated

--- src/pages/Profile/UserProfile.js
+++ src/pages/Profile/UserProfile.js
@@ -25,61 +25,63 @@ const UserProfile = () => {
     <>
       {userProfile !== null && (
         <section>
-          <div className="container px-5 lg:px-0 flex-col font-secondary mx-auto my-5 flex justify-center gap-4">
+          <div className="container px-5 lg:px-0 flex-col font-secondary mx-auto my-5 flex justify-center ">
             {/* Profile Info */}
-            <div className="flex gap-4 justify-start items-center md:items-start flex-col md:flex-row">
-              <div className="user-profile w-28 h-28 rounded-lg flex justify-center items-center text-white font-bold">
-                {profileName}
+            <div className="flex justify-center">
+              <div className="lg:w-[45%] w-full flex gap-4 items-center md:items-start justify-center flex-col md:flex-row px-5 py-8 rounded-lg shadow">
+                <div className="user-profile w-28 h-28 rounded-lg flex justify-center items-center text-white font-bold shadow-md">
+                  {profileName}
+                </div>
+                <table
+                  className="profile-table"
+                  style={{ borderSpacing: "20px", verticalAlign: "top" }}
+                >
+                  <tbody className="text-dark">
+                    <tr>
+                      <td className="font-bold text-gray-500">Name </td>
+                      <td className="pl-3">{userName}</td>
+                    </tr>
+                    <tr>
+                      <td className="font-bold  align-top text-gray-500">
+                        Mail{" "}
+                      </td>
+                      <td className="pl-3">
+                        {userMail} <br />
+                        <span className="text-xs">
+                          {isVerified
+                            ? "Your Email is verified"
+                            : "You are unverified"}
+                          .
+                        </span>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td colSpan={2}>
+                        <button
+                          onClick={() =>
+                            handleCallAlertBox("Are you sure to log out?")
+                          }
+                          className="px-4 py-2  font-bold bg-danger text-white rounded text-xs table ml-auto mt-3"
+                        >
+                          Log Out
+                        </button>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
               </div>
-              <table
-                className="profile-table"
-                style={{ borderSpacing: "20px", verticalAlign: "top" }}
-              >
-                <tbody>
-                  <tr>
-                    <td className="font-bold text-gray-500">Name </td>
-                    <td className="pl-3">{userName}</td>
-                  </tr>
-                  <tr>
-                    <td className="font-bold  align-top text-gray-500">
-                      Mail{" "}
-                    </td>
-                    <td className="pl-3">
-                      {userMail} <br />
-                      <span className="text-xs">
-                        {isVerified
-                          ? "Your Email is verified"
-                          : "You are unverified"}
-                        .
-                      </span>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className="table mx-auto">
-                      <button
-                        onClick={() =>
-                          handleCallAlertBox("Are you sure to log out?")
-                        }
-                        className="px-4 py-2  font-bold bg-danger text-white rounded text-xs"
-                      >
-                        Log Out
-                      </button>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
             </div>
             <div className="w-full">
               {!isAdmin && (
                 <div className="flex flex-col items-center">
-                  <div className="my-5 w-full">
+                  <div className="mt-5 w-full">
                     <PersonalBoards />
 
                     <hr className="my-5" />
                   </div>
                   <Link onClick={handlePersonalBoard} to={"/my-boards/new"}>
                     <button
-                      className={`rounded-md py-2 px-4 my-5 flex justify-center items-center gap-2 font-secondary shadow-md  bg-info text-white`}
+                      className={`rounded-md py-2 px-4 flex justify-center items-center gap-2 font-secondary shadow-md  bg-info text-white`}
                     >
                       Create New Board <RocketIcon />
                     </button>
