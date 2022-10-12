c9e8d69a3247b11e4a527f950a79af0d6d395a3c
T-N9 <thethoriumtension@gmail.com>
2022-10-12 19:03:31 +0630

    [Footer] copyright added

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,9 +5,18 @@ const Footer = () => {
     <footer className="bg-dark py-5">
       <p className="text-center text-white">
         Designed and Built by{" "}
-        <a href="https://www.tenyain.com/" target="_blank" referrerPolicy="noreferrer">
-            <span className="font-secondary font-bold text-light-md">TeNyain</span>
+        <a
+          href="https://www.tenyain.com/"
+          target="_blank"
+          referrerPolicy="noreferrer"
+        >
+          <span className="font-secondary font-bold text-light-md">
+            TeNyain
+          </span>
         </a>
+      </p>
+      <p className="text-center text-white font-secondary">
+      © 2022 Gui-Transpo | All rights reserved.
       </p>
     </footer>
   );
